781a189f99b5547fb83968c205cdafdb35d204a9
pablocabe <pablocabe0@gmail.com>
2025-03-21 16:21:08 -0300

    first commit

---
+++ teoria3.cs
@@ -0,0 +1,10 @@
+﻿double[]? vector = new double[10];
+Procesar(vector, 1, 1);
+Procesar(null, 1, 1);
+
+void Procesar(double[]? v, int i, int c)
+{
+ c = c + 10;
+ v[i] = 1000 / c;
+ Console.WriteLine(v[i]);
+}
