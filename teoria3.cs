4ad840490696229a70e1c0b473e3ab806a7a41f0
pablocabe <pablocabe0@gmail.com>
2025-03-21 17:36:27 -0300

    .

--- teoria3.cs
+++ teoria3.cs
@@ -1,10 +1,40 @@
-﻿double[]? vector = new double[10];
-Procesar(vector, 1, 1);
-Procesar(null, 1, 1);
+﻿using System;
 
-void Procesar(double[]? v, int i, int c)
-{
- c = c + 10;
- v[i] = 1000 / c;
- Console.WriteLine(v[i]);
+Console.WriteLine("Ingrese un numero de ejercicio");
+string? input = Console.ReadLine();
+int ejercicio = int.Parse(input ?? "0");
+switch (ejercicio){
+    case 1:
+        ejercicio1();
+        break; 
+    case 2:
+        ejercicio2();
+        break;
+}
+
+/* Ejercicio 1
+Ejecutar y analizar cuidadosamente el siguiente programa
+Comprobar tipeando teclas y modificadores (shift, ctrl, alt) para apreciar de qué manera se 
+puede acceder a esta información en el código del programa.
+*/
+
+static void ejercicio1(){
+    Console.CursorVisible = false;
+    ConsoleKeyInfo k = Console.ReadKey(true);
+    while (k.Key != ConsoleKey.End){
+    Console.Clear();
+    Console.Write($"{k.Modifiers}-{k.Key}-{k.KeyChar}");
+    k = Console.ReadKey(true);
+    }
+}
+
+/* Ejercicio 2
+Implementar un método para imprimir por consola todos los elementos de una matriz (arreglo de dos 
+dimensiones) pasada como parámetro. Debe imprimir todos los elementos de una fila en la misma línea 
+en la consola.
+Ayuda: Si A es un arreglo, A.GetLength(i) devuelve la longitud del arreglo en la dimensión i.
+*/
+
+static void ejercicio2(){
+    void ImprimirMatriz(double[,] matriz);
 }
