21cf3171ec5e14574710a09c537bf370bd08f13f
pablocabe <pablocabe0@gmail.com>
2025-03-27 15:37:36 -0300

    .

--- obj/Debug/net9.0/teoria3.AssemblyInfo.cs
+++ obj/Debug/net9.0/teoria3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teoria3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62cbfe313aa176bc7fa8a889fa6b6be7f4ca545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7283f99d6bfc7c9d4726bccbbaca26bc0133ac56")]
 [assembly: System.Reflection.AssemblyProductAttribute("teoria3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teoria3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- teoria3.cs
+++ teoria3.cs
@@ -89,7 +89,7 @@ cuadrada generar una excepción ArgumentException
 */
 
 void ejercicio4(){
-    int[,] matriz = new int[,]{{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+    int[,] matriz = new int[,]{{1, 2, 3},{4, 5, 6},{7, 8, 9},{10, 11, 12}};
 
     try
     {
